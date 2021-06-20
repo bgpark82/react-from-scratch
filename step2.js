9b150e2cfa506bc8afe5926fdb5e7bbddb40ebed
bgpark <bgpark82@gmail.com>
2021-06-21 01:22:53 +0900

    step2

--- step2.js
+++ step2.js
@@ -1,7 +1,5 @@
-/** @jsx createElement */
-
-function createElement(type, props = {}, ...children) {
-
+function createElement(type, props, ...children) {
+    
     // type이 함수일 경우 (제일 초기 <App>을 호출 시, 앞에서 정의한 App 함수 자체를 호출한다)
     if (typeof type === "function") {
         return type(null, props, ...children);
@@ -18,14 +16,12 @@ function createElement(type, props = {}, ...children) {
 
 function createDom(element) {
 
-    // element가 텍스트일 경우 텍스트 노드 반환 (주로 제일 마지막 노드)
     if(typeof element === "string") {
         return document.createTextNode(element)
     }
 
     const dom = document.createElement(element.type);
 
-    // 재귀적으로 dom에 자식 노드를 append
     element.props.children
         .map(createDom)
         .forEach(child =>
@@ -35,20 +31,18 @@ function createDom(element) {
 }
 
 function render(element, container) {
-    container.appendChild(createDom(element))
+    const dom = createDom(element)
+    container.appendChild(dom)
 }
 
+/** @jsx createElement */
 function App() {
     return (
-        <div>
-            <h1>안녕하세요</h1>
-        </div>
+        <div className="a">안녕하세요</div>
     );
 }
 
 render(<App/>, document.getElementById("root"))
-
-console.log(<App />)
 
 
 
